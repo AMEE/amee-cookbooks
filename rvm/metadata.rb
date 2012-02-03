3112b09ee14fbf12afc788e39a07a9352860705f
Sam <github@orgraphone.org>
2012-02-03 17:44:57 +0000

    All new fantastic RVM magic

--- rvm/metadata.rb
+++ rvm/metadata.rb
@@ -4,3 +4,5 @@ license          "BSD 3-Clause License"
 description      "Installs/Configures rvm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+depends "nginx"
